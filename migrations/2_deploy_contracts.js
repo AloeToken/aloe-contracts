c891c2759f8d17b9149da3cea34675b3f5961235
AloeSwap <info@aloeswap.com>
2021-06-02 20:25:18 +0200

    Fix big number in deploy script

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -6,7 +6,7 @@ module.exports = async function(deployer) {
   const aloeToken = await AloeToken.deployed();
   await deployer.link(AloeToken, MasterChef);
   const feeAddress = "0x650E5c6071f31065d7d5Bf6CaD5173819cA72c41";
-  const aloePerBlocks = 1000000000000000000;
+  const aloePerBlock = "1000000000000000000";
   const startBlock = 1;
-  await deployer.deploy(MasterChef, aloeToken.address, feeAddress, aloePerBlocks, startBlock);
+  await deployer.deploy(MasterChef, aloeToken.address, feeAddress, aloePerBlock, startBlock);
 };
